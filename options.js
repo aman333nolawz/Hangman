2737b665f895f583ff409f679deaaaed13c8f1e3
aman333nolawz <amananudhiu@gmail.com>
2023-03-11 13:30:40 +0530

    first commit

---
+++ options.js
@@ -0,0 +1,52 @@
+let options = {"Editing":
+  [
+    {
+      "Clue":"Ottal is a Malayalam film (directs) by Jayaraj",
+      "Answer":"directed"
+    },
+    {
+      "Clue":"It is (a) adaptation of the short story Vanka by Anton Chekhov ",
+      "Answer":"an"
+    },
+    {
+      "Clue":"I read the story of Vanka. It was (realy) a touching story",
+      "Answer":"really"
+    },
+    {
+      "Clue":"Vanka (were) cruelly treated in Alyakins house.",
+      "Answer":"was"
+    },
+    {
+      "Clue":"He (feel) very bad and he wrote a letter to his grandfather",
+      "Answer":"felt"
+    },
+    {
+      "Clue":"He (posting) the letter",
+      "Answer":"posted"
+    },
+    {
+      "Clue":"My brother (decide) to stop smoking a few months ago",
+      "Answer":"decided"
+    },
+    {
+      "Clue":"Damu (leave) home a year ago",
+      "Answer":"left"
+    },
+    {
+      "Clue":"My friend (read) a lot of books",
+      "Answer":"reads"
+    },
+    {
+      "Clue": "Vanka is a unhappy orphan who have (been) apprenticed to the shoe maker Alyakhin in Moscow.",
+      "Answer": "has"
+    },
+    {
+      "Clue":"On Christmas Eve, (where) his master was and others are at church",
+      "Answer":"when"
+    },
+    {
+      "Clue":"He ordered South Indian coffee (to) us.",
+      "Answer":"for"
+    },
+  ]
+};
